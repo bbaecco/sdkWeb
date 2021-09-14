708a460490e969f15b91be39867cfb5d5f9ea5ff
bbaecco <kwldusrnclf@naver.com>
2021-09-14 17:57:16 +0900

    Python-Flask json통신 뚫기

--- test.py
+++ test.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World'
 
+@app.route('/echo_call/<param>') #get echo api
+def get_echo_call(param):
+    return jsonify({"param": param})
 
+@app.route('/echo_call', methods=['POST']) #post echo api
+def post_echo_call():
+    param = request.get_json()
+    return jsonify(param)
 
-
+if __name__ == "__main__":
+    app.run()
